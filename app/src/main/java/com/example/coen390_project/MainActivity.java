6ae88449f82dc213e273e29024c34803aea4f7b9
Andrewmcnair52 <andrew-52@hotmail.com>
2019-10-10 16:34:07 -0400

    loggin dem logs (test commit, i've never used android studio integration before)

--- app/src/main/java/com/example/coen390_project/MainActivity.java
+++ app/src/main/java/com/example/coen390_project/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.coen390_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected String Tag = "MainActivity: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(Tag, "onCreate()");
+
+
     }
 }
